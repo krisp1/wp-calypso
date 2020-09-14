8eb572d865d87922f475ea081b23a971a22cd0f1
Dehan <dehandecroos@gmail.com>
2020-09-14 11:18:34 +0530

    Remove variables and references to variables of the AB test parameter isEligibleVariantForDomainTest (#44961)
    * Remove prop isEligibleVariantForDomainTest
    * Further cleanup of dead code and minor style fix in domain suggestion
    * Removed unnecessary parameter and import
    * Fixed issue where the plans selection link was broken due to a missing dependency
    * Hid the domain explainer based on whether the plans selection is available in the flow

--- client/components/domains/domain-registration-suggestion/index.jsx
+++ client/components/domains/domain-registration-suggestion/index.jsx
@@ -252,9 +252,10 @@ class DomainRegistrationSuggestion extends React.Component {
 			comment: 'Shown next to a domain that has a special discounted sale price',
 		} );
 		const infoPopoverSize = isFeatured ? 22 : 18;
+
 		const titleWrapperClassName = classNames( 'domain-registration-suggestion__title-wrapper', {
 			'domain-registration-suggestion__title-domain-copy-test':
-				this.props.isEligibleVariantForDomainTest && ! this.props.isFeatured,
+				this.props.isSignupStep && ! this.props.isFeatured,
 		} );
 
 		return (
@@ -312,7 +313,7 @@ class DomainRegistrationSuggestion extends React.Component {
 
 		let title, progressBarProps;
 		if ( isRecommended ) {
-			title = this.props.isEligibleVariantForDomainTest
+			title = this.props.isSignupStep
 				? translate( 'Our Recommendation' )
 				: translate( 'Best Match' );
 			progressBarProps = {
@@ -331,7 +332,7 @@ class DomainRegistrationSuggestion extends React.Component {
 		}
 
 		if ( title ) {
-			if ( this.props.isEligibleVariantForDomainTest ) {
+			if ( this.props.isSignupStep ) {
 				const badgeClassName = classNames( '', {
 					success: isRecommended,
 					'info-blue': isBestAlternative,
@@ -354,7 +355,7 @@ class DomainRegistrationSuggestion extends React.Component {
 	}
 
 	renderMatchReason() {
-		if ( this.props.isEligibleVariantForDomainTest ) {
+		if ( this.props.isSignupStep ) {
 			return null;
 		}
 
@@ -409,7 +410,7 @@ class DomainRegistrationSuggestion extends React.Component {
 				domainsWithPlansOnly={ domainsWithPlansOnly }
 				onButtonClick={ this.onButtonClick }
 				{ ...this.getButtonProps() }
-				isEligibleVariantForDomainTest={ this.props.isEligibleVariantForDomainTest }
+				isSignupStep={ this.props.isSignupStep }
 				isFeatured={ isFeatured }
 				selectedPaidPlanInSwapFlow={ this.props.selectedPaidPlanInSwapFlow }
 			>
@@ -425,7 +426,7 @@ const mapStateToProps = ( state, props ) => {
 	const productSlug = get( props, 'suggestion.product_slug' );
 	const productsList = getProductsList( state );
 	const currentUserCurrencyCode = getCurrentUserCurrencyCode( state );
-	const stripZeros = props.isEligibleVariantForDomainTest ? true : false;
+	const stripZeros = props.isSignupStep ? true : false;
 	const isPremium = props.premiumDomain?.is_premium || props.suggestion?.is_premium;
 
 	let productCost;
