111d8f784cdd28a0f9767f6d03b427b302166377
Payton Swick <payton@foolord.com>
2020-09-28 20:35:11 -0400

    Fix arguments to useRecordCheckoutLoaded by making args object

--- client/my-sites/checkout/composite-checkout/composite-checkout.js
+++ client/my-sites/checkout/composite-checkout/composite-checkout.js
@@ -549,16 +549,16 @@ export default function CompositeCheckout( {
 		[ couponItem, dataForProcessor, dataForRedirectProcessor, getThankYouUrl, transactionOptions ]
 	);
 
-	useRecordCheckoutLoaded(
+	useRecordCheckoutLoaded( {
 		recordEvent,
-		isInitialCartLoading,
+		isLoadingCart: isInitialCartLoading,
 		isApplePayAvailable,
 		isApplePayLoading,
+		isLoadingStoredCards,
 		responseCart,
 		storedCards,
-		isLoadingStoredCards,
-		productAliasFromUrl
-	);
+		productAliasFromUrl,
+	} );
 
 	const jetpackColors = isJetpackNotAtomic
 		? {
