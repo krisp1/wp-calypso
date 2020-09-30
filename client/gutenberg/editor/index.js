4deef06f51433f744f2af2a0a120751fa0545575
retrofox <rdsuarez@gmail.com>
2020-09-30 13:55:07 -0300

    editor: redirect /block-editor routes

--- client/gutenberg/editor/index.js
+++ client/gutenberg/editor/index.js
@@ -15,12 +15,12 @@ export default function () {
 	page( '/site-editor/:site?', siteSelection, authenticate, siteEditor, makeLayout, clientRender );
 
 	page( '/post', siteSelection, sites, makeLayout, clientRender );
-	page( '/post/:site?', siteSelection, makeLayout, clientRender );
 	page( '/post/:site/:post?', siteSelection, authenticate, post, makeLayout, clientRender );
+	page( '/post/:site?', siteSelection, makeLayout, clientRender );
 
 	page( '/page', siteSelection, sites, makeLayout, clientRender );
-	page( '/page/:site?', siteSelection, makeLayout, clientRender );
 	page( '/page/:site/:post?', siteSelection, authenticate, post, makeLayout, clientRender );
+	page( '/page/:site?', siteSelection, makeLayout, clientRender );
 
 	if ( config.isEnabled( 'manage/custom-post-types' ) ) {
 		page( '/edit/:customPostType', siteSelection, sites, makeLayout, clientRender );
@@ -34,6 +34,25 @@ export default function () {
 		);
 		page( '/edit/:customPostType/:site?', siteSelection, makeLayout, clientRender );
 	}
+
+	// Redirect old block-editor routes.
+	page( '/block-editor/post/', '/post' );
+	page( '/block-editor/post/:site/:post?', ( { params = {} } ) => {
+		const { site, post: postId } = params;
+		if ( postId ) {
+			return page.redirect( `/post/${ site }/${ postId }` );
+		}
+		page.redirect( `/post/${ site }/` );
+	} );
+
+	page( '/block-editor/page/', '/page' );
+	page( '/block-editor/page/:site/:page?', ( { params = {} } ) => {
+		const { site, page: pageId } = params;
+		if ( pageId ) {
+			return page.redirect( `/page/${ site }/${ pageId }` );
+		}
+		page.redirect( `/page/${ site }/` );
+	} );
 
 	page( '/*/*', '/post' );
 	page( '/:site', ( context ) => page.redirect( `/block-editor/post/${ context.params.site }` ) );
